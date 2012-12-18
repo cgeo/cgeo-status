aa22ac5d2f5f05e6895e54cd45d8e2e2d00b43f9
Samuel Tardieu <sam@rfc1149.net>
2012-12-18 13:40:16 +0100

    Add SonaType repository

--- project/Build.scala
+++ project/Build.scala
@@ -8,13 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
       "com.typesafe.akka" % "akka-agent" % "2.0.1",
       "com.mongodb.casbah" %% "casbah" % "2.1.5-1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      resolvers += "Sonatype OSS releases" at "https://oss.sonatype.org/content/repositories/releases/"
     )
 
 }
