6909b15c3ce54f7cff80ab3045f82b4ec7d5d595
Samuel Tardieu <sam@rfc1149.net>
2013-07-09 16:52:03 +0200

    Upgrade Scala, Play and Casbah

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,11 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "com.typesafe.akka" %% "akka-agent" % "2.1.2",
-      "org.mongodb" %% "casbah" % "2.5.0"
+      "org.mongodb" %% "casbah" % "2.6.2"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
+      scalaVersion := "2.10.2",
       resolvers ++= Seq("Sonatype OSS releases" at "https://oss.sonatype.org/content/repositories/releases/",
 			"Sonatype OSS snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/")
     )
