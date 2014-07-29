e32d022d5698f1381e145daec150d6a754a791f3
Samuel Tardieu <sam@rfc1149.net>
2014-07-29 12:13:12 +0200

    Update Scala, sbt, akka, play, and casbah

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,5 @@
 import sbt._
 import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,14 +7,16 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      "com.typesafe.akka" %% "akka-agent" % "2.2.0",
-      "org.mongodb" %% "casbah" % "2.7.0"
+      "com.typesafe.akka" %% "akka-agent" % "2.3.4",
+      "org.mongodb" %% "casbah" % "2.7.3"
     )
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      scalaVersion := "2.10.4",
+    val main = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
+      scalaVersion := "2.11.2",
+      version := appVersion,
       resolvers ++= Seq("Sonatype OSS releases" at "https://oss.sonatype.org/content/repositories/releases/",
-			"Sonatype OSS snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/")
+			"Sonatype OSS snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"),
+      libraryDependencies ++= appDependencies
     )
 
 }
