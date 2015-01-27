cf66e4120641c7f23f5ffe059a6b5f1d9685d84a
Samuel Tardieu <sam@rfc1149.net>
2015-01-27 08:53:00 +0100

    Update Scala, Play and Casbah

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      "org.mongodb" %% "casbah" % "2.7.3"
+      "org.mongodb" %% "casbah" % "2.7.5"
     )
 
     lazy val main = (project in file(".")).enablePlugins(play.PlayScala).settings(
       name := appName,
-      scalaVersion := "2.11.4",
+      scalaVersion := "2.11.5",
       version := appVersion,
       resolvers ++= Seq("Sonatype OSS releases" at "https://oss.sonatype.org/content/repositories/releases/",
 			"Sonatype OSS snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"),
