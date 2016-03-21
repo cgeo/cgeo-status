42d2733e3e6c99dcd9b41078b967251c8a5b5e07
Samuel Tardieu <sam@rfc1149.net>
2016-03-21 15:54:33 +0100

    Remove now-unused status page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,30 +1,11 @@
 package controllers
 
-import com.google.inject.Inject
 import play.api.mvc._
 
-import models._
-
-class Application @Inject() (database: Database) extends Controller {
-
-  private[this] def versionsAndTotal() = {
-    val versions =
-      for (version <- BuildKind.kinds;
-           content <- database.latestVersionFor(version);
-           url = version.url;
-           users = Counters.users(version))
-      yield (content, url, users)
-    val total = versions.collect { case (_, _, Some(n)) => n } .sum
-    (versions, total)
-  }
+class Application extends Controller {
 
   def index = Action {
     MovedPermanently("//www.cgeo.org/status.html")
-  }
-
-  def status = Action {
-    val (versions, total) = versionsAndTotal()
-    Ok(views.html.status(versions, database.getMessage, total))
   }
 
 }
