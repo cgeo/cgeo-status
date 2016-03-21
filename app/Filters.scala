32fe7e3c7752263f2e2bc26abd9125a06d69c3ab
Samuel Tardieu <sam@rfc1149.net>
2016-03-21 16:38:03 +0100

    Remove frame security filters

--- app/Filters.scala
+++ app/Filters.scala
@@ -4,6 +4,6 @@ import play.api.http.HttpFilters
 import play.filters.cors.CORSFilter
 import play.filters.headers.SecurityHeadersFilter
 
-class Filters @Inject() (securityHeadersFilter: SecurityHeadersFilter, corsFilter: CORSFilter) extends HttpFilters {
-  def filters = Seq(securityHeadersFilter, corsFilter)
+class Filters @Inject() (corsFilter: CORSFilter) extends HttpFilters {
+  def filters = Seq(corsFilter)
 }
